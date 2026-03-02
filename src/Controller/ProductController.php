3bdf1672c8efb2aec0fd60d76a68caa2d39e78bf
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-03-02 13:02:10 +0300

    Update src/Controller/ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,8 +13,10 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/product')]
+#[Route('/admin/product')]
+#[IsGranted('ROLE_ADMIN')]
 final class ProductController extends AbstractController
 {
     #[Route(name: 'app_product_index', methods: ['GET'])]
