ddf5254cc05d25cba7f554021da253702a4f26f7
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-03-02 13:02:10 +0300

    Update src/Repository/ProductRepository.php

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -16,6 +16,49 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+    /**
+     * @return Product[]
+     */
+    public function findForStore(?string $query, ?int $categoryId, ?int $subCategoryId): array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->leftJoin('p.subCategories', 'sc')
+            ->leftJoin('sc.category', 'c')
+            ->addSelect('sc', 'c');
+
+        if ($query) {
+            $qb->andWhere('LOWER(p.name) LIKE :q OR LOWER(p.description) LIKE :q')
+                ->setParameter('q', '%' . mb_strtolower($query) . '%');
+        }
+
+        if ($categoryId) {
+            $qb->andWhere('c.id = :categoryId')
+                ->setParameter('categoryId', $categoryId);
+        }
+
+        if ($subCategoryId) {
+            $qb->andWhere('sc.id = :subCategoryId')
+                ->setParameter('subCategoryId', $subCategoryId);
+        }
+
+        return $qb
+            ->orderBy('p.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return Product[]
+     */
+    public function findLatest(int $limit = 8): array
+    {
+        return $this->createQueryBuilder('p')
+            ->orderBy('p.id', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
+
     //    /**
     //     * @return Product[] Returns an array of Product objects
     //     */
