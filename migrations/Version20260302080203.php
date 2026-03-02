cf7f14043e5f2b2a44796c28a856f3a70cc09e90
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-03-02 11:02:33 +0300

    feat: Add Product History

---
+++ migrations/Version20260302080203.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260302080203 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE add_product_history (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, qte INTEGER NOT NULL, created_at DATETIME NOT NULL, product_id INTEGER NOT NULL, CONSTRAINT FK_EDEB7BDE4584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_EDEB7BDE4584665A ON add_product_history (product_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE add_product_history');
+    }
+}
