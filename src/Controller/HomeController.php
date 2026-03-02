694a1671dd7fa94a718b8d81581aa51520558514
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-03-02 13:02:10 +0300

    Update src/Controller/HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'featured_products' => $productRepository->findLatest(6),
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
